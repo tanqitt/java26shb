eca075fff1346e53182898379c03df3a25e55eb9
Administrator <Administrator@SC-201904181800>
2019-08-27 15:08:20 +0800

    第二次

--- src/main/java/com/accp/ErpprojectApplication.java
+++ src/main/java/com/accp/ErpprojectApplication.java
@@ -1,9 +1,14 @@
 package com.accp;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
 
 @SpringBootApplication
+@MapperScan("com.accp.dao")
+@EnableCaching
 public class ErpprojectApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/accp/controller/DepttabController.java
+++ src/main/java/com/accp/controller/DepttabController.java
@@ -2,7 +2,17 @@ package com.accp.controller;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.accp.entity.Depttab;
+import com.accp.service.impl.DepttabServiceImpl;
+import com.alibaba.fastjson.JSON;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -16,6 +26,58 @@ import org.springframework.stereotype.Controller;
 @Controller
 @RequestMapping("/depttab")
 public class DepttabController {
-
+	
+	@Autowired
+	private DepttabServiceImpl depttabServiceImpl;
+	
+	/*
+	 * 查询
+	 */
+	@RequestMapping("/query")	
+	@ResponseBody
+	public List<Depttab> query(){
+		System.out.println("=====================");
+		List<Depttab> list = depttabServiceImpl.list();
+		System.out.println(JSON.toJSONString(list));
+		return list;
+	}
+	
+	/*
+	 * 新增
+	 */
+	
+	@RequestMapping("/insert")	
+	@ResponseBody
+	public List<Depttab> insert(Depttab depttab){
+		System.out.println("=====================");
+		List<Depttab> list = depttabServiceImpl.list();
+		System.out.println(JSON.toJSONString(list));
+		return list;
+	}
+	
+	/*
+	 * 修改
+	 */
+	@RequestMapping("/update")	
+	@ResponseBody
+	public List<Depttab> update(){
+		System.out.println("=====================");
+		List<Depttab> list = depttabServiceImpl.list();
+		System.out.println(JSON.toJSONString(list));
+		return list;
+	}
+	
+	
+	/*
+	 * 删除
+	 */
+	@RequestMapping("/delete")	
+	@ResponseBody
+	public List<Depttab> delete(Integer id){
+		System.out.println("=====================");
+		List<Depttab> list = depttabServiceImpl.list();
+		System.out.println(JSON.toJSONString(list));
+		return list;
+	}
 }
 
